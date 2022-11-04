f3c4eddcf95a552e4a573e0433b27316a0c489db
Seonaid-McNabb <seonaidmcnabb@gmail.com>
2022-11-04 14:40:42 +0100

    Revert "first commit"
    This reverts commit 289982499e481ce8a3a69904cc9bab9ba18fde3b.

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,53 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Journey from './components/Journey'
-import Station from './components/Station'
-import AddJourney from './components/AddJourney'
-import Bikerider from './components/Bikerider'
-import Home from './components/Home'
-import NotFoundPage from './components/Notfoundpage'
-//feature component
-import Login from './components/Login'
+import './App.css';
+import { Routes, Route} from "react-router-dom";
+import Journey from "./components/Journey";
+import Station from "./components/Station";
+import AddJourney from "./components/AddJourney";
+import Bikerider from './components/Bikerider';
+import Home from './components/Home';
+import NotFoundPage from "./components/Notfoundpage";
 //import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   return (
-    <div className='App bg-light'>
-      <Navbar className='bg-success' bg='success' expand='lg'>
-        Welcome to the Helsinki City Bike App
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='me-auto'>
-            <Nav.Link href='/'>Home</Nav.Link>
-            <NavDropdown title='Show statistics' id='basic-nav-dropdown'>
-              <NavDropdown.Item href='journeys'>Bike journeys</NavDropdown.Item>
-              <NavDropdown.Item href='stations'>Bike stations</NavDropdown.Item>
+    <div className="App bg-light">
+
+      <Navbar className="bg-success" bg="success" expand="lg">Welcome to the Helsinki City Bike App
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <NavDropdown title="Show statistics" id="basic-nav-dropdown">
+              <NavDropdown.Item href="journeys">Bike journeys</NavDropdown.Item>
+              <NavDropdown.Item href="stations">
+                Bike stations
+              </NavDropdown.Item>              
             </NavDropdown>
-            <Nav.Link href='addjourney'>Add your bike journey</Nav.Link>
-            <Nav.Link href='ride'>Take a bike ride</Nav.Link>
-            <Nav.Link href='Login'>Login</Nav.Link>
+            <Nav.Link href="addjourney">Add your bike journey</Nav.Link>
+            <Nav.Link href="ride">Take a bike ride</Nav.Link>
+
           </Nav>
         </Navbar.Collapse>
-      </Navbar>
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/addjourney' element={<AddJourney />} />
-        <Route path='/journeys' element={<Journey />} />
-        <Route path='/stations' element={<Station />} />
-        <Route path='/ride' element={<Bikerider />} />
-        <Route path='/*' element={<NotFoundPage />} />
-        <Route path='/Login' element={<Login />} />
-      </Routes>
+    </Navbar> 
+    
+        
+
+
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/addjourney" element={<AddJourney />} />
+      <Route path="/journeys" element={<Journey />}/>
+      <Route path="/stations" element={<Station />}/>
+      <Route path="/ride" element={<Bikerider />}/>
+      <Route path="/*" element={<NotFoundPage />} />
+     </Routes>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Journey.js
+++ client/src/components/Journey.js
@@ -1,209 +1,177 @@
-import React, { useEffect, useState } from 'react'
-import { Table } from './Table'
-import Plotly from 'plotly.js-dist-min'
-import '../App.css'
-const { DateTime } = require('luxon')
+import React, {useEffect, useState } from 'react';
+import { Table } from "./Table";
+import Plotly from 'plotly.js-dist-min';
+import '../App.css';
+const { DateTime } = require("luxon");
+
+
 
 export default function Journey() {
-  const [journeys, setJourneys] = useState([])
 
-  useEffect(() => {
-    getJourneys()
-  }, [])
+  const [journeys, setJourneys] = useState([]);
+  
+   useEffect(() => {
+    getJourneys();
+   }, []);
 
-  const getJourneys = () => {
+   const getJourneys = () => {
+   
     fetch('/journeys')
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json)
-        setJourneys(json)
-      })
+    .then(res => res.json())
+    .then(json => {
+      console.log(json);
+      setJourneys(json);
+    })      
+  
+   .catch(error => {
+    console.log(error);
+    
+   });
+   }   
 
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-
-  const showStatistics = async () => {
-    var xArray = journeys.map((journey) => journey.CoveredDistance / 1000)
-    var yArray = journeys.map((journey) => journey.Duration / 60)
+   const showStatistics = async() => {
+    
+    var xArray = journeys.map(journey => journey.CoveredDistance/1000);
+    var yArray = journeys.map(journey => journey.Duration/60);
     // console.log("Xarray", xArray);
     // console.log("Yarray",yArray);
     // Define Data
-    var data = []
-    data = [
-      {
-        x: xArray,
-        y: yArray,
-        mode: 'markers',
-        type: 'scatter',
-      },
-    ]
+    var data = [];
+    data = [{
+      x: xArray,
+      y: yArray,
+      mode:"markers",
+      type:"scatter"
+    }];
 
     // Define Layout
     var layout = {
-      xaxis: {
-        range: [0, Math.max(...xArray) * 1.2],
-        title: 'Covered distance, km',
-      },
-      yaxis: { range: [0, Math.max(...yArray) * 1.2], title: 'Duration, min' },
-      title: 'Covered distances',
-    }
+      xaxis: {range: [0, Math.max(...xArray)*1.2], title: "Covered distance, km"},
+      yaxis: {range: [0, Math.max(...yArray)*1.2], title: "Duration, min"},
+      title: "Covered distances"
+    };
 
-    Plotly.newPlot('plotDistance', data, layout)
-  }
+    Plotly.newPlot('plotDistance',data, layout);
 
-  const showStationStats = async () => {
-    const row = document.getElementsByClassName('highlight')
-    // the id of the row to be displayed
-    let rowID = row[0].getAttribute('id')
-    console.log(rowID)
-    let journey = journeys.filter((e) => e.id === `${rowID}`)
-    console.log(journey[0].departureStationName)
+   }
 
-    let filterDeparture = journeys.filter(
-      (e) => e.departureStationName === journey[0].departureStationName
-    )
-    let filterReturn = journeys.filter(
-      (e) => e.returnStationName === journey[0].returnStationName
-    )
-    let depHours = filterDeparture.map(
-      (journey) =>
-        parseInt(DateTime.fromISO(journey.departureTime).toFormat('HH')) - 3
-    )
-    let retHours = filterDeparture.map(
-      (journey) =>
-        parseInt(DateTime.fromISO(journey.returnTime).toFormat('HH')) - 3
-    )
+   const showStationStats = async () => {
+    const row = document.getElementsByClassName('highlight');
+   // the id of the row to be displayed
+    let rowID = row[0].getAttribute('id');
+    console.log(rowID);
+    let journey = journeys.filter(e => e.id == `${rowID}`);
+    console.log(journey[0].departureStationName);
 
-    let depHoursRetStation = filterReturn.map(
-      (journey) =>
-        parseInt(DateTime.fromISO(journey.departureTime).toFormat('HH')) - 3
-    )
-    let retHoursRetStation = filterReturn.map(
-      (journey) =>
-        parseInt(DateTime.fromISO(journey.returnTime).toFormat('HH')) - 3
-    )
+    let filterDeparture = journeys.filter(e => e.departureStationName === journey[0].departureStationName);
+    let filterReturn = journeys.filter(e => e.returnStationName === journey[0].returnStationName);
+    let depHours = filterDeparture.map(journey => parseInt(DateTime.fromISO(journey.departureTime).toFormat('HH'))-3);
+    let retHours = filterDeparture.map(journey => parseInt(DateTime.fromISO(journey.returnTime).toFormat('HH'))-3);
 
-    /// histogram with dropdown menus
+    let depHoursRetStation = filterReturn.map(journey => parseInt(DateTime.fromISO(journey.departureTime).toFormat('HH'))-3);
+    let retHoursRetStation = filterReturn.map(journey => parseInt(DateTime.fromISO(journey.returnTime).toFormat('HH'))-3);  
+  
+   
+    /// histogram with dropdown menus 
 
-    let dataToPlot = [
-      depHours,
-      retHours,
-      depHoursRetStation,
-      retHoursRetStation,
-    ]
-    let nameForPlot = [
-      `Departure hours for departure station ${journey[0].departureStationName}`,
-      ...`Return hours for departure station ${journey[0].returnStationName}`,
-      ...`Departure hours for return station ${journey[0].returnStationName}`,
-      ...`Return hours for return station ${journey[0].returnStationName}`,
-    ]
-    console.log(nameForPlot)
+    let dataToPlot = [depHours, retHours,depHoursRetStation, retHoursRetStation];
+    let nameForPlot = [`Departure hours for departure station ${journey[0].departureStationName}`,
+    ...`Return hours for departure station ${journey[0].returnStationName}`,
+    ...`Departure hours for return station ${journey[0].returnStationName}`,
+    ...`Return hours for return station ${journey[0].returnStationName}`]
+    console.log(nameForPlot);
 
     function makeTrace(i) {
       return {
-        y: dataToPlot[i],
-        type: 'histogram',
-        visible: i === 0,
-        name: nameForPlot[i],
-        xaxis: { title: 'Hours' },
-      }
+          y: dataToPlot[i], 
+          type: "histogram",     
+          visible: i === 0,
+          name: nameForPlot[i],
+          xaxis: {title: "Hours"}
+
+      };
     }
 
     Plotly.newPlot('myDiv', [0, 1, 2, 3].map(makeTrace), {
-      updatemenus: [
-        {
+      updatemenus: [{
           y: 0.8,
           yanchor: 'top',
-          buttons: [
-            {
+          buttons: [{
               method: 'restyle',
               args: ['line.color', 'red'],
-              label: `return station ${journey[0].returnStationName}`,
-            },
-            {
+              label: `return station ${journey[0].returnStationName}`
+          }, {
               method: 'restyle',
               args: ['line.color', 'blue'],
-              label: `departure station ${journey[0].departureStationName}`,
-            },
-          ],
-        },
-        {
+              label: `departure station ${journey[0].departureStationName}`
+          }]
+      }, {
           y: 1,
           yanchor: 'top',
-          buttons: [
-            {
+          buttons: [{
               method: 'restyle',
               args: ['visible', [true, false, false, false]],
-              label: 'return hours',
-            },
-            {
+              label: 'return hours'
+          }, {
               method: 'restyle',
               args: ['visible', [false, true, false, false]],
-              label: 'departure hours',
-            },
-          ],
-        },
-      ],
-    })
-  }
+              label: 'departure hours'
+          }]
+      }],
+    });
+      }
 
-  const deleteJourney = async () => {
-    // find id with class "highlight"
-    const row = document.getElementsByClassName('highlight')
-    console.log(
-      'This is id of the row to be deleted',
-      row[0].getAttribute('id')
-    )
-    let rowID = row[0].getAttribute('id')
-    // delete a journey from the database
-    const response = await fetch(`/journeys/${rowID}`, {
-      method: 'DELETE',
-    })
-    const journey = await response.json()
-    setJourneys(journey)
-    alert('Journey deleted successfully!')
-  }
 
-  console.log('This is a journey object: ', journeys[0])
 
-  const columns = [
+   const deleteJourney = async () => {
+   // find id with class "highlight"
+   const row = document.getElementsByClassName('highlight');
+   console.log("This is id of the row to be deleted", row[0].getAttribute('id'));
+   let rowID = row[0].getAttribute('id');
+   // delete a journey from the database
+  const response = await fetch(`/journeys/${rowID}`, {
+    method: "DELETE"    
+   });   
+     const journey = await response.json();
+     setJourneys(journey); 
+     alert("Journey deleted successfully!");   
+   } 
+
+   console.log("This is a journey object: ", journeys[0]);
+
+   const columns = [
     { accessor: 'id', label: 'ID' },
-    { accessor: 'departureTime', label: 'Depature time' },
+    { accessor: 'departureTime', label: 'Depature time'},
     { accessor: 'returnTime', label: 'Return time' },
     { accessor: 'departureStationName', label: 'Departure station name' },
-    { accessor: 'returnStationName', label: 'Return station name' },
-    { accessor: 'CoveredDistance', label: 'Covered distance(m)' },
-    { accessor: 'Duration', label: 'Duration(sec)' },
+    { accessor: 'returnStationName', label: 'Return station name'},
+    { accessor: 'CoveredDistance', label: 'Covered distance(m)'},
+    { accessor: 'Duration', label: 'Duration(sec)'}
   ]
 
   return (
-    <div
-      style={{
-        backgroundImage: `url("https://images.unsplash.com/photo-1531685250784-7569952593d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2FsbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60")`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        width: '100vw',
-        height: '100vh',
-      }}
-    >
-      <h2 className='text-center'>Bike journey statistics</h2>
 
-      <Table rows={journeys} columns={columns} deleteClick={deleteJourney} />
+    <div style={{ 
+      backgroundImage: `url("https://images.unsplash.com/photo-1531685250784-7569952593d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2FsbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60")` ,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      width: '100vw',
+      height: '100vh'
+    
+    }}>
 
-      <button onClick={showStatistics}>Show statistics</button>
-      <button onClick={showStationStats}>
-        Show statistics on the selected stations
-      </button>
+    <h2  className='text-center'>Bike journey statistics</h2>     
+        
+        <Table rows={journeys} columns={columns} deleteClick={deleteJourney}/>  
 
-      <h2 id='plotDistance' className='plot'>
-        Statistics on covered distance and duration
-      </h2>
-      <br />
-      <h2 id='myDiv' className='plot'>
-        Statistics on selected departure and return stations
-      </h2>
+        <button onClick={showStatistics}>Show statistics</button>
+        <button onClick={showStationStats}>Show statistics on the selected stations</button> 
+      
+        <h2 id='plotDistance' className='plot'>Statistics on covered distance and duration</h2> 
+        <br/> 
+        <h2 id='myDiv'className='plot'>Statistics on selected departure and return stations</h2>      
+
+
     </div>
   )
 }
